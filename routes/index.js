3940a6c53f2c335a7ecd642ca91cb68b549e7466
nobleteam112 <demerakalpha@gmail.com>
2019-05-31 15:36:38 -0500

    modificacion de datos hagen

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,43 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+router.get('/Kiana', function(req, res, next) {
+  res.render('Kiana', { title: 'Express' });
+});
+router.get('/Oswaldo', function(req, res, next) {
+  res.render('Oswaldo', { title: 'Express' });
+});
+router.get('/Hagen', function(req, res, next) {
+  res.render('Hagen', { title: 'Express' });
+});
+router.get('/A1-0', function(req, res, next) {
+  res.render('A1-0', { title: 'Express' });
+});
+router.get('/A1-1', function(req, res, next) {
+  res.render('A1-1', { title: 'Express' });
+});
+router.get('/A1-2', function(req, res, next) {
+  res.render('A1-2', { title: 'Express' });
+});
+router.get('/A2-0', function(req, res, next) {
+  res.render('A2-0', { title: 'Express' });
+});
+router.get('/A2-1', function(req, res, next) {
+  res.render('A2-1', { title: 'Express' });
+});
+router.get('/A2-2', function(req, res, next) {
+  res.render('A2-2', { title: 'Express' });
+});
+router.get('/A3-0', function(req, res, next) {
+  res.render('A3-0', { title: 'Express' });
+});
+router.get('/A3-1', function(req, res, next) {
+  res.render('A3-1', { title: 'Express' });
+});
+router.get('/A3-2', function(req, res, next) {
+  res.render('A3-2', { title: 'Express' });
+});
+router.get('/A3-3', function(req, res, next) {
+  res.render('A3-3', { title: 'Express' });
+});
 module.exports = router;
